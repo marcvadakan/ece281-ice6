e7f23da1c3c01ac7c1d1e431b2f875ea3219f360
marcvadakan <225086998+marcvadakan@users.noreply.github.com>
2026-03-19 16:26:45 -0600

    submission

--- src/TDM4_tb.vhd
+++ src/TDM4_tb.vhd
@@ -54,29 +54,48 @@ architecture test_bench of TDM4_tb is
   
 	component TDM4 is
 		-- fill in from TDM4.vhd
-
+        generic ( constant k_WIDTH : natural := 4 );
+		Port ( i_clk   : in  STD_LOGIC;
+		       i_reset : in  STD_LOGIC;
+		       i_D3    : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		       i_D2    : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		       i_D1    : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		       i_D0    : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		       o_data  : out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		       o_sel   : out STD_LOGIC_VECTOR (3 downto 0)
+		);
 	end component TDM4;
 
 	-- Setup test clk (20 ns --> 50 MHz) and other signals
 	
 	-- Constants
 	constant k_IO_WIDTH : natural := 4;
+	constant k_clk_period : time    := 20 ns;
 	-- Signals
-	
+	signal w_clk   : STD_LOGIC := '0';
+	signal w_reset : STD_LOGIC := '0';
+ 
+	signal w_D3    : STD_LOGIC_VECTOR (k_IO_WIDTH - 1 downto 0) := (others => '0');
+	signal w_D2    : STD_LOGIC_VECTOR (k_IO_WIDTH - 1 downto 0) := (others => '0');
+	signal w_D1    : STD_LOGIC_VECTOR (k_IO_WIDTH - 1 downto 0) := (others => '0');
+	signal w_D0    : STD_LOGIC_VECTOR (k_IO_WIDTH - 1 downto 0) := (others => '0');
+ 
+	signal f_data  : STD_LOGIC_VECTOR (k_IO_WIDTH - 1 downto 0);
+	signal f_sel_n : STD_LOGIC_VECTOR (3 downto 0);
 begin
 	-- PORT MAPS ----------------------------------------
 	-- map ports for any component instances (port mapping is like wiring hardware)
 	uut_inst : TDM4 
-	generic map ( k_WIDTH =>  )
+	generic map ( k_WIDTH =>  k_IO_WIDTH)
 	port map ( 
-       i_clk   => 
-       i_reset => 
-       i_D3    => 
-       i_D2    => 
-       i_D1    => 
-       i_D0    => 
-       o_data  => 
-       o_sel   => 
+       i_clk   => w_clk,
+       i_reset => w_reset,
+       i_D3    => w_D3,
+       i_D2    => w_D2,
+       i_D1    => w_D1,
+       i_D0    => w_D0,
+       o_data  => f_data,
+       o_sel   => f_sel_n
 	);
 	-----------------------------------------------------	
 	
@@ -84,9 +103,10 @@ begin
 	-- Clock Process ------------------------------------
 	clk_process : process
 	begin
-
-
-
+        w_clk <= '0';
+		wait for k_clk_period / 2;
+		w_clk <= '1';
+		wait for k_clk_period / 2;
 	end process clk_process;
 	-----------------------------------------------------	
 	
@@ -94,7 +114,10 @@ begin
 	test_process : process 
 	begin
 		-- assign test values to data inputs
-
+        w_D3 <= "1100";
+		w_D2 <= "1001";
+		w_D1 <= "0110";
+		w_D0 <= "0011";
 				
 		-- reset the system first
 		w_reset <= '1';
